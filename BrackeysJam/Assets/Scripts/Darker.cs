9f979bdf42a7057115433dba43165f7f1e44a10a
Yohann Degli Esposti <yohann.degli-esposti@epita.fr>
2019-02-16 18:22:42 +0100

    [BJ][LIGHT] first tests with lightning.

---
+++ BrackeysJam/Assets/Scripts/Darker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Darker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        RenderSettings.ambientSkyColor = Color.black;
+        RenderSettings.ambientLight = Color.black;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ BrackeysJam/Assets/Scripts/HorizontalMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMove : MonoBehaviour {
+
+    float speed = 10f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float DirX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.position = new Vector2(transform.position.x + DirX, transform.position.y);
+	}
+}
