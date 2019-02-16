5d72476cdb1fd063a76d5491836e4571d7d090f6
Yohann Degli Esposti <yohann.degli-esposti@epita.fr>
2019-02-16 17:31:07 +0100

    [BJ][MOVE] now having the basic movement up and down 3

---
+++ BrackeysJam/Assets/Scripts/ChangeGravity.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeGravity : MonoBehaviour {
+
+    bool isInverted;
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 1f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       if (Input.GetKey(KeyCode.Space))
+        {
+            if (!isInverted)
+            {
+                isInverted = true;
+            }
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 10, 0);
+        }
+
+        else
+        {
+            
+            if (isInverted)
+            {
+                isInverted = false;
+            }
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, -10, 0);
+        }
+	}
+}
