28a6667c82c7b4b3cd140cf2f91bf1969de83b05
Yohann Degli Esposti <yohann.degli-esposti@epita.fr>
2019-02-18 13:11:19 +0100

    [BJ][GUIDING] after testing a minimap, we decided to implement a little UI feature (an arrow) that would indicate the position of the lover. It disappears if the lover is seen.

---
+++ BrackeysJam/Assets/Scripts/LookTowardsLover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookTowardsLover : MonoBehaviour {
+
+    public GameObject lover;
+    void Update()
+    {
+        Vector3 diff = lover.transform.position - transform.position;
+        diff.Normalize();
+
+        float rotZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
+    }
+}
